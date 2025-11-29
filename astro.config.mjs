dc185f5e29988486d5247c9fe72839bb8df3734a
CristhianCG <cristhiancg9x@gmail.com>
2025-11-28 21:44:21 -0500

    Corrigiendo configuracion del sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'; // <--- Asegúrate de que esto esté aquí
 
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://quillaelectric.site',
+  // Tienes que poner tu dominio EXACTO (con https)
+  site: 'https://quillaelectric.site', 
+  
+  // Aquí activamos el plugin del mapa
   integrations: [sitemap()],
 });
